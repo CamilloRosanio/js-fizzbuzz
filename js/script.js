a3d4f43aa8ea661b10b30cfbd1a732572edeae47
MyUsername <rosanio.c@gmail.com>
2024-10-15 13:36:49 +0200

    impostazione template

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+
+// creo un FOR incrementale di numeri da 1 a 100
+
+// devo verificare se il numero è divisibile per 3 AND per 5
+    // SE il numero è divisibile secondo criterio
+    // ALLORA preparo l'output "fizzbuzz"
+
+// SE il numero non è divisibile per 3 AND per 5
+    // ALLORA verifico se il numero è divisibile per 3
+    // E preparo l'output "fizz"
+
+// SE il numero non è divisibile per 3
+    // ALLORA verifico se il numero è divisibile per 5
+    // E preparo l'output "buzz"
+
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+    console.log(`Ripetizione: ${i} - Numero: ${i}`)
+
+}
